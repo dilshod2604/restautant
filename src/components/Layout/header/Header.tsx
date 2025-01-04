9222d7a63cf517351d70c2748ce17c83946fccf7
sajkal <sajkal949@gmail.com>
2025-01-04 14:24:29 +0600

    created about

--- src/components/Layout/header/Header.tsx
+++ src/components/Layout/header/Header.tsx
@@ -5,16 +5,33 @@ import { links } from "@/src/constants/linkxs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { IoSearchOutline } from "react-icons/io5";
+import { useLanguageStore } from "@/src/stores/useLanguageStore";
+import { li } from "framer-motion/client";
 
 const Header: FC = () => {
-  const [showMenu, setShowMenu] = useState(false); 
+  const [showMenu, setShowMenu] = useState(false);
   const router = useRouter();
+  const { t, setLanguage, language } = useLanguageStore();
+  const hrefs = [
+    { name: t("Интерьер", "Интерьер", "Interior"), href: "interior" },
+    { name: t("Биз жонундо", "О нас", "About Us"), href: "about" },
+    { name: t("Меню", "Меню", "Menu"), href: "/menu" },
+    { name: t("Контакты", "Контакты", "Contacts"), href: "contacts" },
+  ];
+
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedLanguage = e.target.value as "ky" | "ru" | "en";
+    setLanguage(selectedLanguage);
+  };
 
   return (
     <section className={scss.Header}>
       <div className="container">
         <div className={scss.content}>
-          <h1 onClick={() => router.push("/")}>Restaurant</h1>
+          <h1 onClick={() => router.push("/")}>
+            {" "}
+            {t("Ресторан", "Ресторан", "Restaurant")}
+          </h1>
           <div className={scss.menu}>
             <nav className={scss.navigation}>
               {links.map((link) => (
@@ -28,13 +45,17 @@ const Header: FC = () => {
               <input
                 className={scss.searchInput}
                 type="text"
-                placeholder="search..."
+                placeholder={t("Издөө", "Поиск", "Search")}
               />
             </div>
-            <select className={scss.select}>
-              <option value="">EN</option>
-              <option value="">RU</option>
-              <option value="">KG</option>
+            <select
+              onChange={handleLanguageChange}
+              value={language}
+              className={scss.select}
+            >
+              <option value="en">EN</option>
+              <option value="ru">RU</option>
+              <option value="ky">KG</option>
             </select>
           </div>
           <div className={scss.burger} onClick={() => setShowMenu(!showMenu)}>
