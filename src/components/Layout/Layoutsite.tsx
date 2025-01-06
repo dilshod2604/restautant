8907ad2065eafc5b2b86c9f31743002e3fbb435b
unknown <dilshodadilbekov456@gmail.com>
2025-01-06 09:36:18 +0600

    features/create_serch_page is finished

--- src/components/Layout/Layoutsite.tsx
+++ src/components/Layout/Layoutsite.tsx
@@ -1,18 +1,26 @@
-import React, { ReactNode } from "react";
+"use client";
+import React, { ReactNode, useEffect, useState } from "react";
 
 import scss from "./LayoutSite.module.scss";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-
+import Prealoder from "../UI/preloader/Prealoder";
 
 const Layoutsite = ({ children }: { children: ReactNode }) => {
+  const [isPreloader, setIsPreloader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsPreloader(false);
+    }, 4000);
+  }, []);
 
+  if (isPreloader) return <Prealoder />;
   return (
     <div className={scss.LayoutSite}>
       <Header />
-      
+
       <main>{children}</main>
-      <Footer />  
+      <Footer />
     </div>
   );
 };
